05ae6bbc3edc7f442e2af80dc10b0fdaca6230f2
Andong <475289076@qq.com>
2012-07-18 17:13:54 -0400

    update logo

---
+++ McvTest1/McvTest1/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace McvTest1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        /*
+        public String  Index()
+        {
+            return "Hello World! <br />";
+        }
+        */
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
